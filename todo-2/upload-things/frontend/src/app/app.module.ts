3558f026c228193c8cc55598721108321ed414d5
Vazgen Ayrapetyan <vazgen.ayrapetyan@united.com>
2022-04-21 14:45:17 +0200

    started something

---
+++ todo-2/upload-things/frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateProfileComponent } from './components/create-profile/create-profile.component';
+import { AllProfilesComponent } from './components/all-profiles/all-profiles.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateProfileComponent,
+    AllProfilesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
